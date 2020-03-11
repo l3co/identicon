2403aff819b3b2be667532cdb154349809e27ba9
leco <lcosta.sp.br@gmail.com>
2020-03-11 09:18:31 -0300

    feature: create hash_input, update documentation

--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -2,7 +2,9 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
-  test "greets the world" do
-    assert Identicon.hello() == :world
+  defp banana_hash, do: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
+
+  test "generate hash" do
+    assert Identicon.hash_input("banana") == banana_hash()
   end
 end
